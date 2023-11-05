562b01ba54c22bc985b1d09c5d7b65d1a0d669a0
vladoja <dr.jalo2@gmail.com>
2023-11-05 19:48:41 +0100

    SizedTransition

--- lib/size_transition/main.dart
+++ lib/size_transition/main.dart
@@ -10,7 +10,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: const Color.fromARGB(255, 154, 199, 221),
+      ),
       home: const AnimatedSizeExample(),
     );
   }
@@ -23,16 +26,61 @@ class AnimatedSizeExample extends StatefulWidget {
   State<AnimatedSizeExample> createState() => _AnimatedSizeExampleState();
 }
 
-class _AnimatedSizeExampleState extends State<AnimatedSizeExample> {
+class _AnimatedSizeExampleState extends State<AnimatedSizeExample>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 1000))
+      ..forward()
+      ..repeat(reverse: true);
+    _animation =
+        CurvedAnimation(parent: _controller, curve: Curves.fastOutSlowIn);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Animated Size Example'),
+        title: const Text('Size Transition Example'),
         centerTitle: true,
       ),
       body: Center(
-        child: const Text('Test'),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizeTransition(
+              sizeFactor: _animation,
+              axis: Axis.vertical,
+              axisAlignment: 0,
+              child: Center(
+                child: Container(
+                  width: 200,
+                  height: 200,
+                  color: Color.fromARGB(255, 82, 132, 220),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.black38,
+            ),
+          ],
+        ),
       ),
     );
   }
